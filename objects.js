72a0fabbd8c38092f82022744dce0841da96c61b
Kayla Golder <golderkayla@gmail.com>
2021-10-27 00:20:29 -0400

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,6 @@
+// create an object fruits with at least 5 fruits that have the name of the fruit as the key and the value being the color of the fruit
+// print out each value in that object manually
+
+//then print out each value using a for loop
+
+// write a function that takes in an object and key and returns that value at that key in the object
